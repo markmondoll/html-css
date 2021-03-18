710a499a63e141d9af8c7fa43b078f69872a44ce
markmondoll <markmondol@gmail.com>
2021-03-18 22:26:02 +0600

    Top Button Complete

--- js/app.js
+++ js/app.js
@@ -23,3 +23,23 @@ menu_item.forEach((item) => {
     mobile_menu.classList.toggle('active'); 
   });
 });
+
+//Get the button
+var top_button = document.getElementById("topBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    top_button.style.display = "block";
+  } else {
+    top_button.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
